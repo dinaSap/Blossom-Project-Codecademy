8c151f7d8c2a3703fb5b1fbd49aafc2e4f201eb5
dinaSap <49748840+dinaSap@users.noreply.github.com>
2020-04-26 19:43:11 +0400

    Create script.py

---
+++ script.py
@@ -0,0 +1,37 @@
+from linked_list import Node, LinkedList
+from blossom_lib import flower_definitions 
+
+class HashMap:
+  def __init__(self, size):
+    self.array_size = size
+    self.array = [LinkedList() for i in range(self.array_size)]
+  def hash(self, key):
+    key_bytes = key.encode()
+    hash_code = sum(key_bytes)
+    return hash_code
+  def compress(self, hash_code):
+    return hash_code % self.array_size
+  def assign(self, key, value):
+    array_index = self.compress(self.hash(key))
+    
+    payload = Node([key, value])
+    list_at_array = self.array[array_index]
+    for i in list_at_array:
+      if i[0] == key:
+        i[1] = value
+    list_at_array.insert(payload)
+        
+  def retrieve(self, key):
+    array_index = self.compress(self.hash(key))
+    list_at_index = self.array[array_index]
+    for i in list_at_index:
+      if i[0] == key:
+        return i[1]
+      else:
+        return None
+blossom = HashMap(len(flower_definitions))
+for i in flower_definitions:
+  blossom.assign(i[0], i[1])
+print(blossom.retrieve('daisy'))
+      
+    
